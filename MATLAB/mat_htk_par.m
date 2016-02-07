c0aa2a8c3e71ee00d486062cc5de021b94a86b60
Thrasyvoulos Karydis <karydis@mit.edu>
2016-02-07 17:18:13 -0500

    tuning

--- MATLAB/mat_htk_par.m
+++ MATLAB/mat_htk_par.m
@@ -1,39 +1,45 @@
-clear all;
+%% Speech separation and recognition from an HMM Trained on TIMIT copurs by HTK
+% 
+% University of Patras - Anastasia Grigoropoulou 2016
+
+clear;
 clc;
-load('filters.mat');
+load('legacy/filters.mat');
+% Set up directories
+HOME = '../';
+WORKDIR = strcat(HOME,'HTK_TIMIT_WRD/');
+TIMIT = strcat(HOME, 'TIMIT/TIMIT/');
+
+%% Loading files
+%load the input signals
+fid = fopen('coreTEST.SCP');
+files = textscan(fid,'%s\n');
+files = files{1};
+fclose(fid);
+
+% traindir = 'matlab_examples/'; 
+nFiles = 2;
+inputs = cell(2,1);
+fs = 16000;
+for i=1:nFiles
+    [inputs{i}, Fs] = read_NIST_file(strcat(TIMIT,files{i}));
+    assert(Fs==fs);
+end
+
+s = zeros(nFiles,max(cellfun('length',inputs)));
+
+for i=1:nFiles
+    s(i,1:size(inputs{i},2)) = inputs{i};
+end
 
 %% without extra noise
-%load the input signals
-traindir = 'matlab_examples/'; 
-nFiles = 2;
-s = [];
- 
-[y1,fs] = read_NIST_file('matlab_examples/SI1024.WAV');
-size_1 = length(y1);
- 
-[y2,fs] = read_NIST_file('matlab_examples/SI943.WAV');
-size_2 = length(y2);
-s=zeros(2,max(size_1,size_2));
-s(1,1:size_1) = y1;
-s(2,1:size_2) = y2;
- 
-%  for i=1:nFiles
-%     filename = sprintf('SA%d.WAV',i);
-%     [y,fs] = read_NIST_file(strcat(traindir,filename));
-%      %s(i,:) = y;
-%      %s(1,:) = filter(Hd,y);
-%      s(i,:)= y(round(fs/2):round(5*fs/2));
-%  end
- 
-% hear the input signals
-sound(s(1,:),fs);
-sound(s(2,:),fs);
  
 %plot the input signals
-figure;
+inputs = figure;
 subplot(2,1,1); plot(s(1,:)),grid on, title('signal_1'), xlabel('t (msec)'); % plot s1
 subplot(2,1,2); plot(s(2,:), 'r'),grid on, title('signal_2'), xlabel('t (msec)'); % plot s2
- 
+
+
 %create the mixing table and mix the signals
  A=[0.5 0.3; 0.8 0.9];  
  x=[];
