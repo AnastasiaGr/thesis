343aa7f8e6d9e635545f2e490fca938d1b626052
Anastasia <anastacy@gmail.com>
2016-02-16 00:10:59 +0200

    Stacy files

--- MATLAB/eval_matlab.sh
+++ MATLAB/eval_matlab.sh
@@ -19,10 +19,10 @@ INPUTS=${PROJECT}/MATLAB/Inputs
 
 
 cd ${OUTPUTS}
-ls *.wav > temp
-sed 's/.wav/.mfc/' < temp > MATLAB.MFC
-sed 's/.wav/.rec/' < temp > MATLAB.REC
-paste temp MATLAB.MFC > MATLAB.SCP
+#ls *.wav > temp
+#sed 's/.wav/.mfc/' < temp > MATLAB.MFC
+#sed 's/.wav/.rec/' < temp > MATLAB.REC
+#paste temp MATLAB.MFC > MATLAB.SCP
 
 DIR=${HMM}/hmm62/tri-nmix28-npass4
 p=-2.0
@@ -30,6 +30,4 @@ s=13.0
 
 #HCopy -A -T 1 -S MATLAB.SCP -C ${CONFIG}/configMATLAB
 #HVite -A -T 1 -S MATLAB.MFC -C ${CONFIG}/configCROSS -H ${DIR}/MMF -w ${LMODEL}/timit_lm/wdnet_ug -t 250.0 -p ${p} -s ${s} ${DICT}/dict ${WORKDIR}/tiedlist
-HResults -A -T 1 -t -c -S MATLAB.REC -I MATLABWord.mlf ${WORKDIR}/tiedlist > log.results
-
-
+#HResults -A -T 1 -t -c -S MATLAB.REC -I MATLABWord.mlf ${WORKDIR}/tiedlist > log.results
